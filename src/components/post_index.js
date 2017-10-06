8b25e72e8110e3585dc1e8ae80c9a2e87ff52880
Joe Williamson <jhwilliamson@outlook.com>
2017-10-06 19:42:39 -0400

    api call map

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -3,16 +3,33 @@ import { connect } from 'react-redux';
 import bindActionCreators from 'react-redux'
 import { fetchPosts } from '../actions';
 
-
+import _ from 'lodash';
 
 class PostIndex extends Component {
 componentDidMount() {
   this.props.fetchPosts()  
 } 
+
+renderPosts() {
+     return _.map(this.props.posts, post => {
+         return (
+             <li className="list-group-itme" key={post.id}>{post.content}</li>
+     )
+    })
     
+}  
+
+
+
 render() {
+    console.log(this.props.posts)
   return (
-    <div>Posts Index Test One</div>
+    <div>
+      <h3>Posts</h3>
+      <ul>
+      {this.renderPosts()}
+      </ul>
+      </div>
   )};
 }
 
@@ -21,4 +38,8 @@ render() {
 //    bindActionCreators({fetchPost}, dispatch)
 //}
 
-export default connect(null, { fetchPosts })(PostIndex);
+function mapStateToProps(state) {
+    return {posts: state.posts}
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostIndex);
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-import promise from 'redux-promise'
+import promise from 'redux-promise';
 
 import reducers from './reducers';
-import PostIndex from './components/post_index'
+import PostIndex from './components/post_index';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
