8946f00e38858df4e6461ccf43227fd441e82d32
Joe Williamson <jhwilliamson@outlook.com>
2017-10-06 13:14:51 -0400

    action wire-up

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import bindActionCreators from 'react-redux'
+import { fetchPosts } from '../actions';
+
+
 
 class PostIndex extends Component {
- render() {
-    return (
+componentDidMount() {
+  this.props.fetchPosts()  
+} 
+    
+render() {
+  return (
     <div>Posts Index Test One</div>
   )};
 }
 
-export default PostIndex;
 
+//function mapDispatchToProps(dispatch){
+//    bindActionCreators({fetchPost}, dispatch)
+//}
+
+export default connect(null, { fetchPosts })(PostIndex);
